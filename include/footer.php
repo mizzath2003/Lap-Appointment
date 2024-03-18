4b22ac1f097f4245eebc1b9584ada0dc3406aff7
Muhammed Izzath <mizzath2003@gmail.com>
2024-03-18 14:02:48 +0530

    Sign in & Login

--- include/footer.php
+++ include/footer.php
@@ -68,7 +68,7 @@
                 <p><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
                     Copyright &copy;<script>
                         document.write(new Date().getFullYear());
-                    </script> All rights reserved | HealthCrew
+                    </script> All rights reserved | Med Labs
                     <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
                 </p>
             </div>
--- login.php
+++ login.php
@@ -16,19 +16,13 @@ require "components/navbar.php";
     <div class="container mt-5 pt-5">
         <div class="row justify-content-center mb-5 pb-2 mt-5">
             <div class="col-md-8 text-center heading-section ftco-animate">
-                <h2 class="mb-4">Sign Up</h2>
+                <h2 class="mb-4">Login</h2>
             </div>
         </div>
         <div class="row align-items-center">
             <div class="mx-auto">
-                <form onsubmit="return verifyPasswords()" action="dbh/userRegister" method="POST" class="d-flex flex-column justify-content-center align-items-center row g-3 needs-validation " novalidate>
-                    <div class="col-md-5 ">
-                        <label for="validationCustom01" class="form-label colorBlack">Name</label>
-                        <input type="text" class="form-control" id="validationCustom01" name="name" required>
-                        <div class="invalid-feedback">
-                            Please enter your name.
-                        </div>
-                    </div>
+                <form onsubmit="return verifyPasswords()" action="dbh/userLogin" method="POST" class="d-flex flex-column justify-content-center align-items-center row g-3 needs-validation " novalidate>
+
                     <div class="col-md-5 ">
                         <label for="validationCustomUsername" class="form-label colorBlack">Email</label>
                         <div class="input-group has-validation">
@@ -38,13 +32,7 @@ require "components/navbar.php";
                             </div>
                         </div>
                     </div>
-                    <div class="col-md-5 ">
-                        <label for="validationCustom08" class="form-label colorBlack">Mobile Number</label>
-                        <input type="text" class="form-control" name="mobile" id="validationCustom08" required>
-                        <div class="invalid-feedback">
-                            Please enter a valid mobile number.
-                        </div>
-                    </div>
+
                     <div class="col-md-5 ">
                         <label for="pass1" class="form-label colorBlack">Password</label>
                         <input type="password" class="form-control" id="pass1" name="password" required onkeyup="hideError()">
@@ -56,9 +44,9 @@ require "components/navbar.php";
 
 
                     <div class="d-grid gap-2 mb-3 mt-5 col-5 mx-auto">
-                        <button type="submit" class="btn btn-primary btn-lg">Sign Up</button>
+                        <button type="submit" class="btn btn-primary btn-lg">Sign In</button>
                     </div>
-                    <p class="text-center">Already have an account? <a href="login.php">Login</a></p>
+                    <p class="text-center">Don't have an account? <a href="signup.php">Sign Up</a></p>
                     <div class="mt-2 alert alert-danger" role="alert" id="error" style="visibility:hidden">
                         Passwords do not match
                     </div>
